21f125cc42edd2059023459b41a9320861fd796c
WitheringAway <124115470+notwithering@users.noreply.github.com>
2026-05-15 22:58:25 -0400

    add progressbar example and gitignore

---
+++ examples/progressbar/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+
+	"github.com/notwithering/kdialog"
+)
+
+func main() {
+	db := kdialog.DialogBox{
+		Form:    kdialog.ProgressBar,
+		Text:    "Loading...",
+		Minimum: 0,
+		Maximum: 1000,
+	}
+	bar := db.MustRun().(*kdialog.ProgressBarResult)
+
+	for i := range db.Maximum + 1 {
+		if c, _ := bar.Cancelled(); c {
+			return
+		}
+
+		bar.SetProgress(i)
+		time.Sleep(2 * time.Second / time.Duration(db.Maximum))
+	}
+
+	time.Sleep(time.Second)
+	bar.Quit()
+}
