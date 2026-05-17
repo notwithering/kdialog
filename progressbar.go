ca5984cf4ee3260f78be9d5d011e5e4e0b3bde72
WitheringAway <124115470+notwithering@users.noreply.github.com>
2026-05-17 13:44:08 -0400

    rename `progressbarresult.go` to `progressbar.go`

---
+++ progressbar.go
@@ -0,0 +1,56 @@
+package kdialog
+
+import (
+	"github.com/godbus/dbus/v5"
+)
+
+type ProgressBar struct {
+	obj dbus.BusObject
+}
+
+const (
+	errNoExist = "The name is not activatable"
+)
+
+func (p ProgressBar) SetProgress(prog int) error {
+	call := p.obj.Call("org.freedesktop.DBus.Properties.Set", 0, "org.kde.kdialog.ProgressDialog", "value", dbus.MakeVariant(prog))
+	if call.Err != nil {
+		if call.Err.Error() == errNoExist {
+			return nil
+		}
+		return call.Err
+	}
+
+	return nil
+}
+
+func (p ProgressBar) Cancelled() (bool, error) {
+	var cancelled bool
+
+	call := p.obj.Call("org.kde.kdialog.ProgressDialog.wasCancelled", 0)
+	if call.Err != nil {
+		if call.Err.Error() == errNoExist {
+			return true, nil
+		}
+		return false, call.Err
+	}
+
+	err := call.Store(&cancelled)
+	if err != nil {
+		return false, err
+	}
+
+	return cancelled, err
+}
+
+func (p ProgressBar) Quit() error {
+	err := p.obj.Call("org.kde.kdialog.ProgressDialog.close", 0).Err
+	if err != nil {
+		if err.Error() == errNoExist {
+			return nil
+		}
+		return err
+	}
+
+	return nil
+}
